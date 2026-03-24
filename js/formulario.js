460b2c4d8e90531cad14162adac7e9e993c49ea7
nlaot-ai <nlaot@ucenfotec.ac.cr>
2026-03-23 23:48:33 -0600

    [init]: Tarea2, Validación de formulario con JS

---
+++ js/formulario.js
@@ -0,0 +1,43 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.querySelector(".formulario");
+
+  formulario.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const confirmar = document.getElementById("confirmar").value.trim();
+
+ 
+    if (nombre === "" || password === "" || confirmar === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favior, completa todos los campos",
+      });
+      return;
+    }
+
+
+    if (password !== confirmar) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Las contraseñas no coinciden",
+      });
+      return;
+    }
+
+
+    Swal.fire({
+      icon: "success",
+      title: "Cuenta creada",
+      text: "El usuario se registró correctamente",
+    });
+
+
+    formulario.reset();
+  });
+});
